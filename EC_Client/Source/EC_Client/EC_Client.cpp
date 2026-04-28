b7233e270683537d53d94d48ff242479ee76810d
GeonRyoung <rjsfud7467@naver.com>
2026-04-28 09:35:43 +0900

    프로젝트 수정 계획

--- EC_Client/Source/EC_Client/EC_Client.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "EC_Client.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EC_Client, "EC_Client" );
--- ES_Server/ES_Server/Player.cpp
+++
@@ -1,5 +0,0 @@
-#include "pch.h"
-#include "Player.h"
-
-Player::Player(int32_t profileId, string nickname, int32_t gold, int32_t level)
-    : _profileId(profileId), _nickname(nickname), _gold(gold), _level(level) {}
