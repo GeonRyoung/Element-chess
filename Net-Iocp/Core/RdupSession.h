eca9bfd24927994e1a77c1740e2f50be9d68983e
GeonRyoung <rjsfud7467@naver.com>
2026-05-26 09:16:36 +0900

    feat:rudp base

---
+++ Net-Iocp/Core/RdupSession.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#pragma comment(lib, "Ws2_32.lib")
+
+#include <WinSock2.h>
+#include <atomic>
+#include <memory>
+
+enum class SessionState : uint8_t
+{
+    Free = 0,       // LockFreeObjectPool 내부 대기
+    Connecting,     // 연결 중
+    WaitAuth,       // 인증 대기
+    Active,         // 활성화
+    Closed          // 종료
+};
+
+class RudpSession : public std::enable_shared_from_this<RudpSession>
+{
+private:
+    SOCKET m_socket;
+    uint64_t m_sessionId;
+    
+    std::atomic<SessionState> m_state;
+    
+    WSAOVERLAPPED m_recvOverlapped;
+    WSAOVERLAPPED m_sendOverlapped;
+public:
+    RudpSession();
+    virtual ~RudpSession();
+    
+    void Init(SOCKET socket, uint64_t sessionId);
+    
+    void DisConnect();
+    
+    SessionState GetState() const {return m_state.load(std::memory_order_acquire);}
+    bool SetState(SessionState expectedState, SessionState newState);
+    
+    void PostRecv();
+    void PostSend();
+};
